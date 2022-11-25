9fad82dc7ad211e4e4923f562b8c81ce9550fea1
niejakub02 <73759797+niejakub02@users.noreply.github.com>
2022-11-25 17:46:27 +0100

    EEC-07 Home Page structure (#5)
    * Home Page structure v1
    
    * EEC-07 Home Page structure v1.1
    
    * EEC-07 Home Page structure v1.2
    
    * Home Page structure v1.3
    
    * EEC-07 Home Page structure v1.3
    
    * EEC-07 Home Page structure v1.4
    
    Co-authored-by: niejakub02 <imaslanky@gmail.com>

---
+++ src/app/components/HomePageComponents/ecoScore/ecoScore.tsx
@@ -0,0 +1,14 @@
+import './EcoScore.scss'
+
+export const EcoScore: React.FC = () => {
+  return (
+    <div className="eco-score">
+      <div className="eco-score__labels">
+        <h3>Your ECO score</h3>
+        <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+      </div>
+
+      <div className="eco-score__container" />
+    </div>
+  )
+}
---
+++ src/app/components/HomePageComponents/improvement/improvement.tsx
@@ -0,0 +1,13 @@
+import './Improvement.scss'
+
+export const Improvement: React.FC = () => {
+  return (
+    <div className="improvement">
+      <div className="improvement__labels">
+        <h2>Welcome Back, *Username* 👋</h2>
+        <h4>Suggested Changes</h4>
+      </div>
+      <div className="improvement__container" />
+    </div>
+  )
+}
