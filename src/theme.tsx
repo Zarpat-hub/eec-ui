abd6ad7c4d5e3fb40af090c3dd6ca29fe1991691
niejakub02 <imaslanky@gmail.com>
2022-11-25 10:47:22 +0100

    EEC-10 React MUI theme v1.1

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,39 +1,39 @@
-import { createTheme } from '@mui/material';
-import variables from './variables.module.scss';
+import { createTheme } from '@mui/material'
+import variables from './variables.module.scss'
 
 declare module '@mui/material/styles' {
-    interface BreakpointOverrides {
-      xs: false;
-      sm: false;
-      md: false;
-      lg: false;
-      xl: false;
-      mobile: true;
-      tablet: false;
-      laptop: false;
-      desktop: true;
-    }
+  interface BreakpointOverrides {
+    xs: false
+    sm: false
+    md: false
+    lg: false
+    xl: false
+    mobile: true
+    tablet: false
+    laptop: false
+    desktop: true
+  }
 }
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            dark: variables.primaryBlack,
-            main: variables.primaryBlue,
-            light: variables.primaryGrey
-        },
-        secondary: {
-            dark: variables.accentVibrantBlue,
-            main: variables.secondaryBlue,
-            light: variables.accentBlue
-        },
+  palette: {
+    primary: {
+      dark: variables.primaryBlack,
+      main: variables.primaryBlue,
+      light: variables.primaryGrey,
     },
-    breakpoints: {
-        values: {
-            mobile: Number(variables.mobile.slice(0, -2)),
-            desktop: Number(variables.desktop.slice(0, -2))
-        }
-    }
+    secondary: {
+      dark: variables.accentVibrantBlue,
+      main: variables.secondaryBlue,
+      light: variables.accentBlue,
+    },
+  },
+  breakpoints: {
+    values: {
+      mobile: Number(variables.mobile.slice(0, -2)),
+      desktop: Number(variables.desktop.slice(0, -2)),
+    },
+  },
 })
 
-export default theme;
+export default theme
