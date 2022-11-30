7573678c209c71850ecc1600dc79f7507f6090cd
kwlodarczyk0 <wlodarczyk.krystian00@gmail.com>
2022-11-30 16:55:45 +0100

    EEC-13 implementation

---
+++ src/app/components/Shared/NewItem/NewItem.tsx
@@ -0,0 +1,11 @@
+import './NewItem.scss'
+
+export const NewItem = () => {
+  return (
+    <div className="newItem">
+      <span className="newItem__sign">
+        <span>+</span>
+      </span>
+    </div>
+  )
+}
