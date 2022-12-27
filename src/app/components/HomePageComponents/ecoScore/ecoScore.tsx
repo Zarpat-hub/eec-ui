c8bb28ce8e89afbb56c2e776fde51d49131a5359
niejakub02 <imaslanky@gmail.com>
2022-12-27 21:37:03 +0100

    EEC-22 savings calcs

--- src/app/components/HomePageComponents/ecoScore/ecoScore.tsx
+++ src/app/components/HomePageComponents/ecoScore/ecoScore.tsx
@@ -1,13 +1,45 @@
 import { useDevices } from '../../../context/DevicesContext'
 import { DEVICE } from '../../Shared/models/Device'
 import Ecometer from '../ecometer/Ecometer'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './ecoScore.scss'
 import NoEnoughData from '../../../../assets/not_enough_data.png'
+import upgradeArrow from '../../../../assets/upgrade_arrow.png'
 
 export const EcoScore: React.FC = () => {
-  const { devices } = useDevices()
+  const { devices, suggestedDevice, activeDevice } = useDevices()
   const [ecoScore, setEcoScore] = useState<number>(0)
+  const [spendings, setSpendings] = useState<number>(0)
+  const [moneySavings, setMoneySavings] = useState<number>(0)
+  const [moneyPercentages, setMoneyPercentages] = useState<number>(0)
+  const [energyUsed, setEnergyUsed] = useState<number>(0)
+  const [energyReduced, setEnergyReduced] = useState<number>(0)
+  const [energyPercentages, setEnergyPercentages] = useState<number>(0)
+  const moneySavingsRef = useRef<any>()
+  const moneyPercentagesRef = useRef<any>()
+  const spendingsArrowRef = useRef<any>()
+  const spendingsRef = useRef<any>()
+  const energyUsedRef = useRef<any>()
+  const energyUsedArrowRef = useRef<any>()
+  const energyReducedRef = useRef<any>()
+  const energyPercentagesRef = useRef<any>()
+
+  useEffect(() => {
+    if (suggestedDevice === undefined) {
+      noSuggestion()
+      return
+    }
+
+    suggestion()
+    const [x1, x2, x3, x4] = spendingsAndEnergyCalc(devices)
+
+    setSpendings(x1)
+    setMoneySavings(x2)
+    setMoneyPercentages(Number(((x1 - x2) / x1).toFixed(2)))
+    setEnergyUsed(x3)
+    setEnergyReduced(x4)
+    setEnergyPercentages(Number(((x3 - x4) / x3).toFixed(2)))
+  }, [suggestedDevice, devices])
 
   useEffect(() => {
     if (devices.length === 0) {
@@ -18,6 +50,38 @@ export const EcoScore: React.FC = () => {
     setEcoScore(e)
   }, [devices])
 
+  const spendingsAndEnergyCalc = (devices: DEVICE[]): number[] => {
+    if (devices.length === 0) return [0, 0]
+    let spendings = 0
+    let savings = 0
+    let energyUsed = 0
+    let energyReduced = 0
+
+    for (const device of devices) {
+      if (
+        device.modelIdentifier === activeDevice.modelIdentifier &&
+        suggestedDevice !== undefined
+      ) {
+        savings += Number(suggestedDevice.annualCost)
+        spendings += Number(activeDevice.annualCost)
+        energyReduced += Number(suggestedDevice.powerConsumption)
+        energyUsed += Number(activeDevice.powerConsumption)
+      } else {
+        savings += Number(device.annualCost)
+        spendings += Number(device.annualCost)
+        energyReduced += Number(device.powerConsumption)
+        energyUsed += Number(device.powerConsumption)
+      }
+    }
+
+    return [
+      Math.floor(spendings / devices.length),
+      Math.floor(savings / devices.length),
+      Math.floor(energyUsed / devices.length),
+      Math.floor(energyReduced / devices.length),
+    ]
+  }
+
   const ecoScoreCalc = (devices: DEVICE[]): number => {
     let ecoScoreDefault = 0
 
@@ -26,6 +90,80 @@ export const EcoScore: React.FC = () => {
     }
 
     return Math.floor(ecoScoreDefault / devices.length)
+  }
+
+  const noSuggestion = () => {
+    moneySavingsRef.current.animate(
+      [{ transform: 'translateY(33%) translateX(-50%)', opacity: 0 }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    moneyPercentagesRef.current.animate(
+      [{ transform: 'translateY(-33%) translateX(-50%)', opacity: 0 }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    spendingsArrowRef.current.animate([{ opacity: 0 }], {
+      duration: 500,
+      fill: 'forwards',
+      easing: 'ease-in-out',
+    })
+    spendingsRef.current.animate(
+      [{ transform: 'translateX(125%)', fontSize: '20px' }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    energyReducedRef.current.animate(
+      [{ transform: 'translateY(33%) translateX(-50%)', opacity: 0 }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    energyPercentagesRef.current.animate(
+      [{ transform: 'translateY(-33%) translateX(-50%)', opacity: 0 }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    energyUsedArrowRef.current.animate([{ opacity: 0 }], {
+      duration: 500,
+      fill: 'forwards',
+      easing: 'ease-in-out',
+    })
+    energyUsedRef.current.animate(
+      [{ transform: 'translateX(125%)', fontSize: '20px' }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+  }
+
+  const suggestion = () => {
+    moneySavingsRef.current.animate(
+      [{ transform: 'translateY(0)', opacity: 1 }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    moneyPercentagesRef.current.animate(
+      [{ transform: 'translateY(0)', opacity: 1 }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    spendingsArrowRef.current.animate([{ opacity: 1 }], {
+      duration: 500,
+      fill: 'forwards',
+      easing: 'ease-in-out',
+    })
+    spendingsRef.current.animate(
+      [{ transform: 'translateX(0)', fontSize: '16px' }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    energyReducedRef.current.animate(
+      [{ transform: 'translateY(0)', opacity: 1 }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    energyPercentagesRef.current.animate(
+      [{ transform: 'translateY(0)', opacity: 1 }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
+    energyUsedArrowRef.current.animate([{ opacity: 1 }], {
+      duration: 500,
+      fill: 'forwards',
+      easing: 'ease-in-out',
+    })
+    energyUsedRef.current.animate(
+      [{ transform: 'translateX(0)', fontSize: '16px' }],
+      { duration: 500, fill: 'forwards', easing: 'ease-in-out' }
+    )
   }
 
   // console.log(devices.length)
@@ -48,8 +186,54 @@ export const EcoScore: React.FC = () => {
                   </p>
                 </div>
                 <div className="header__savings savings">
-                  <div className="savings__money eco-score-card--border-right" />
-                  <div className="savings__energy" />
+                  <div className="savings__money money eco-score-card--border-right">
+                    <span className="money__current" ref={spendingsRef}>
+                      {spendings},-
+                    </span>
+                    <img
+                      className="money__img"
+                      src={upgradeArrow}
+                      ref={spendingsArrowRef}
+                    />
+                    <div className="money-savings">
+                      <span
+                        className="money-savings__savings"
+                        ref={moneySavingsRef}
+                      >
+                        {moneySavings},-
+                      </span>
+                      <span
+                        className="money-savings__percentages"
+                        ref={moneyPercentagesRef}
+                      >
+                        +{moneyPercentages}%
+                      </span>
+                    </div>
+                  </div>
+                  <div className="savings__energy energy">
+                    <span className="energy__current" ref={energyUsedRef}>
+                      {energyUsed},-
+                    </span>
+                    <img
+                      className="energy__img"
+                      src={upgradeArrow}
+                      ref={energyUsedArrowRef}
+                    />
+                    <div className="energy-savings">
+                      <span
+                        className="energy-savings__savings"
+                        ref={energyReducedRef}
+                      >
+                        {energyReduced},-
+                      </span>
+                      <span
+                        className="energy-savings__percentages"
+                        ref={energyPercentagesRef}
+                      >
+                        +{energyPercentages}%
+                      </span>
+                    </div>
+                  </div>
                 </div>
               </div>
 
