17fccb5190d35ae2c8da46bc5ae541b1a1715fae
kwlodarczyk0 <wlodarczyk.krystian00@gmail.com>
2022-12-28 23:05:01 +0100

    resolve

--- src/app/components/LandingPageComponents/About_Section/AboutProject.tsx
+++ src/app/components/LandingPageComponents/About_Section/AboutProject.tsx
@@ -10,31 +10,45 @@ export const AboutProject: React.FC = () => {
         <h2 className="about__header">Your ECO household starts today 🍃</h2>
         <h3 className="about__ndHeader">How we do it</h3>
         <p className="about__paragraph">
-        Welcome to Echo, the Energy Efficiency Assistant! Are you tired of high energy bills and looking for ways to save money on your monthly expenses? Our easy-to-use tool allows you to assess the energy efficiency of your home or business and provides personalized recommendations on how to improve it. 
+          Welcome to Echo, the Energy Efficiency Assistant! Are you tired of
+          high energy bills and looking for ways to save money on your monthly
+          expenses? Our easy-to-use tool allows you to assess the energy
+          efficiency of your home or business and provides personalized
+          recommendations on how to improve it.
         </p>
         <div className="about__cards">
           <div className="card">
             <TimelineIcon className="card__icon" />
-            <h4 className="card__header">Smart Assistant. Smart Home. Smart Life.</h4>
+            <h4 className="card__header">
+              Smart Assistant. Smart Home. Smart Life.
+            </h4>
             <p className="card__description">
-            Our Energy Efficiency Assistant will analyze your information and generate a customized report with recommendations on how to improve the energy efficiency of your property.
+              Our Energy Efficiency Assistant will analyze your information and
+              generate a customized report with recommendations on how to
+              improve the energy efficiency of your property.
             </p>
           </div>
           <div className="card">
             <MenuIcon className="card__icon" />
             <h4 className="card__header">Our commitment to sustainability.</h4>
             <p className="card__description">
-            At Echo, we believe that small changes can make a big difference when it comes to energy efficiency and sustainability. That's why we're committed to helping homeowners and businesses reduce their energy usage and carbon footprint.
+              At Echo, we believe that small changes can make a big difference
+              when it comes to energy efficiency and sustainability. That is why
+              we are committed to helping homeowners and businesses reduce their
+              energy usage and carbon footprint.
             </p>
           </div>
         </div>
         <div className="card--full">
           <HomeIcon className="card__icon" />
-          <h3 className="card__header">
-          The benefits of using our tool.
-          </h3>
+          <h3 className="card__header">The benefits of using our tool.</h3>
           <p className="card__description">
-          In addition to saving money on your energy bills and reducing your carbon footprint, improving the energy efficiency of your home or business can also increase the value of your property and improve its overall comfort level. Our tool is designed to be user-friendly and easy to use, providing clear, actionable recommendations that are tailored specifically to your property.
+            In addition to saving money on your energy bills and reducing your
+            carbon footprint, improving the energy efficiency of your home or
+            business can also increase the value of your property and improve
+            its overall comfort level. Our tool is designed to be user-friendly
+            and easy to use, providing clear, actionable recommendations that
+            are tailored specifically to your property.
           </p>
         </div>
       </div>
