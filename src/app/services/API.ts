dbb1785cccfd810684f8d76a995d4fd4fc168694
kwlodarczyk0 <wlodarczyk.krystian00@gmail.com>
2023-01-04 19:01:45 +0100

    api

--- src/app/services/API.ts
+++ src/app/services/API.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://localhost:5200/api/',
-  // baseURL: 'https://eec-api.herokuapp.com/api/',
+  // baseURL: 'http://localhost:5200/api/',
+  baseURL: 'https://eec-api.herokuapp.com/api/',
 })
 
 export default client
